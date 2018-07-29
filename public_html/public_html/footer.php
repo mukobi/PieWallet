180eb001ca341e18e6edff51ac5dcf880c5935d8
Mukobi <gabrielmukobi@gmail.com>
2018-07-29 12:35:11 -0700

    Import existing code

---
+++ public_html/public_html/footer.php
@@ -0,0 +1,12 @@
+</main>
+<script type="text/javascript" src="js/convert-currency.js?ver=<?php echo time(); ?>"></script>
+<script type="text/javascript" src="js/faq-accordian.js"></script>
+<script type="text/javascript" src="js/app_js.js?ver=<?php echo time(); ?>"></script>
+<script type="text/javascript" src="js/functions.js?ver=<?php echo time(); ?>"></script>
+<?php $conn->close(); ?> 
+<script src="js/wow.min.js"></script>
+<script>
+new WOW().init();
+</script>
+</body>
+</html>
