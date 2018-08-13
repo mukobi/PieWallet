1244ee7a4ecc490ff9b04bcbceb0dbc666667ad7
Mukobi <gabrielmukobi@gmail.com>
2018-08-12 22:07:24 -0700

    Finish looks of flickety and rest of mobile design, add placeholder transactions

--- index.php
+++ index.php
@@ -269,7 +269,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>' ?>
 				</div>
 			</div>
 			<div id="transactions" class="dashbox genbox balancebox">
-				<h3>Balance and Transaction History</h3>
+				<h3>Transaction History</h3>
 				</br>
 				<div id="balance-content">
 					<?php
@@ -295,6 +295,100 @@ echo '<?xml version="1.0" encoding="utf-8"?>' ?>
 						echo "Please login to see your balance";
 					}
 					?>
+				</div>
+				<div class="placeholder-history">
+					<ul class="balancetable">
+						<li>
+							<p>Transaction 1</p>
+						</li>
+						<li>
+							<p>Transaction 2</p>
+						</li>
+						<li>
+							<p>Transaction 3</p>
+						</li>
+						<li>
+							<p>Transaction 4</p>
+						</li>
+						<li>
+							<p>Transaction 5</p>
+						</li>
+						<li>
+							<p>Transaction 6</p>
+						</li>
+						<li>
+							<p>Transaction 7</p>
+						</li>
+						<li>
+							<p>Transaction 8</p>
+						</li>
+						<li>
+							<p>Transaction 9</p>
+						</li>
+						<li>
+							<p>Transaction 10</p>
+						</li>
+						<li>
+							<p>Transaction 11</p>
+						</li>
+						<li>
+							<p>Transaction 12</p>
+						</li>
+						<li>
+							<p>Transaction 13</p>
+						</li>
+						<li>
+							<p>Transaction 14</p>
+						</li>
+						<li>
+							<p>Transaction 15</p>
+						</li>
+						<li>
+							<p>Transaction 16</p>
+						</li>
+						<li>
+							<p>Transaction 17</p>
+						</li>
+						<li>
+							<p>Transaction 18</p>
+						</li>
+						<li>
+							<p>Transaction 19</p>
+						</li>
+						<li>
+							<p>Transaction 20</p>
+						</li>
+						<li>
+							<p>Transaction 21</p>
+						</li>
+						<li>
+							<p>Transaction 22</p>
+						</li>
+						<li>
+							<p>Transaction 23</p>
+						</li>
+						<li>
+							<p>Transaction 24</p>
+						</li>
+						<li>
+							<p>Transaction 25</p>
+						</li>
+						<li>
+							<p>Transaction 26</p>
+						</li>
+						<li>
+							<p>Transaction 27</p>
+						</li>
+						<li>
+							<p>Transaction 28</p>
+						</li>
+						<li>
+							<p>Transaction 29</p>
+						</li>
+						<li>
+							<p>Transaction 30</p>
+						</li>
+					</ul>
 				</div>
 			</div>
 			<div id="sendreceive" class="dashbox">
