9a9118f8540e2571239af1d5d75965641c62dc84
Mukobi <gabrielmukobi@gmail.com>
2018-08-14 19:01:15 -0700

    Embed changelly for exchange

--- components/exchange/exchange.php
+++ components/exchange/exchange.php
@@ -1,3 +1,3 @@
 <div id="exchange" class="dashbox">
-    Exchange
+    <iframe src="https://changelly.com/widget/v1?auth=email&from=BTC&to=ETH&merchant_id=p738adshgy1jyxu4&address=&amount=1&ref_id=p738adshgy1jyxu4&color=00cf70" width="600" height="500" class="changelly" scrolling="yes" style="overflow-x: hidden; border: none" > Can't load widget </iframe>
 </div>
