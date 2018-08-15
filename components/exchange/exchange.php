1d6366eaae55c7f059bff9663ab0db292b075717
Mukobi <gabrielmukobi@gmail.com>
2018-08-14 20:13:33 -0700

    Update merchant ID for changelly

--- components/exchange/exchange.php
+++ components/exchange/exchange.php
@@ -1,3 +1,3 @@
 <div id="exchange" class="dashbox">
-    <iframe src="https://changelly.com/widget/v1?auth=email&from=BTC&to=ETH&merchant_id=p738adshgy1jyxu4&address=&amount=1&ref_id=p738adshgy1jyxu4&color=00cf70" width="600" height="500" class="changelly" scrolling="yes" style="overflow-x: hidden; border: none" > Can't load widget </iframe>
+    <iframe src="https://changelly.com/widget/v1?auth=email&from=LTC&to=BTC&merchant_id=b6b98ece50c8&address=&amount=1&ref_id=b6b98ece50c8&color=8b00cf" width="100%" height="100%" class="changelly" scrolling="auto" style="overflow-x: hidden; border: none" > Can't load widget </iframe>
 </div>
