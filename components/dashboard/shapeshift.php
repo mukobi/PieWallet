18aa65a8959103d3ce8e3b4f253e49cf4e7334c3
Mukobi <gabrielmukobi@gmail.com>
2018-08-16 00:25:04 -0700

    Remove initial deposit quantity of 3 from shapeshift embed

--- components/dashboard/shapeshift.php
+++ components/dashboard/shapeshift.php
@@ -1,3 +1,3 @@
 <div id="shapeshift" class="tab dashbox">
-    <iframe src="https://litespeed-uc2.herokuapp.com/#/?youHave=3&youGet=15.34369212&youHave_c=ETH&youGet_c=LTC" width="100%" height="100%" class="shapeshift" scrolling="auto" style="overflow-x: hidden; border: none" > Can't load widget </iframe>
+    <iframe src="https://litespeed-uc2.herokuapp.com/#/?youHave_c=ETH&youGet_c=LTC" width="100%" height="100%" class="shapeshift" scrolling="auto" style="overflow-x: hidden; border: none" > Can't load widget </iframe>
 </div>
