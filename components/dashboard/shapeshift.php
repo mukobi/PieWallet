be5c5c29f3806f2ab3d1f529fcfea18258e62ba0
Mukobi <gabrielmukobi@gmail.com>
2018-08-16 00:15:07 -0700

    Implement dashboard shapely widget

--- components/dashboard/shapeshift.php
+++ components/dashboard/shapeshift.php
@@ -1,24 +1,3 @@
-<div id="shapeshift" class="dashbox">
-    <h3>ShapeShift</h3>
-    <div class="genbox buybox">
-        <h4 class="buybox">Buy Cryptocurrencies</h4>
-        <a id="changellyButton" href="https://changelly.com/widget/v1?auth=email&from=USD&to=BTC&merchant_id=b6b98ece50c8&address=&amount=100&ref_id=b6b98ece50c8&color=4977f1"
-            target="_blank">
-            <img src="https://changelly.com/pay_button_buy_sell.png" />
-        </a>
-        <div id="changellyModal">
-            <div class="changellyModal-content">
-                <span class="changellyModal-close">x</span>
-                <iframe src="https://changelly.com/widget/v1?auth=email&from=USD&to=BTC&merchant_id=b6b98ece50c8&address=&amount=100&ref_id=b6b98ece50c8&color=4977f1"
-                    width="600" height="500" class="changelly" scrolling="no" style="overflow-y: hidden; border: none">
-                    Can't load widget
-                </iframe>
-            </div>
-            <script type="text/javascript">
-                var changellyModal = document.getElementById('changellyModal');
-                var changellyButton = document.getElementById('changellyButton');
-                var changellyCloseButton = document.getElementsByClassName('changellyModal-close')[0];
-            </script>
-        </div>
-    </div>
+<div id="shapeshift" class="tab dashbox">
+    <iframe src="https://litespeed-uc2.herokuapp.com/#/?youHave=3&youGet=15.34369212&youHave_c=ETH&youGet_c=LTC" width="100%" height="100%" class="shapeshift" scrolling="auto" style="overflow-x: hidden; border: none" > Can't load widget </iframe>
 </div>
