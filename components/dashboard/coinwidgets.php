a31f820fbd121060908caf5d8a27dd11a91139ae
Mukobi <gabrielmukobi@gmail.com>
2018-08-18 23:48:50 -0700

    Make ticker filler text and style

--- components/dashboard/coinwidgets.php
+++ components/dashboard/coinwidgets.php
@@ -4,17 +4,32 @@
             <img class="coin-logo" src="images/Bitcoin-logo.png" alt="Bitcoin Logo">
             <h4>BTC</h34>
         </div>
+        <div class="ticker-main">
+            <h3>$8,573.00</h3>
+            <h4 class="change">+6.84%</h4>
+            <h4>Balance: 4.39320</h4>
+        </div>
     </div>
     <div id="widget-litecoin" class="coin-widget dashbox carousel-cell">
         <div class="coin-label">
             <img class="coin-logo" src="images/Litecoin-logo.png" alt="Litecoin Logo">
             <h4>LTC</h4>
         </div>
+        <div class="ticker-main">
+            <h3>$164.00</h3>
+            <h4 class="change">+2.73%</h4>
+            <h4>Balance: 98.2857</h4>
+        </div>
     </div>
     <div id="widget-ethereum" class="coin-widget dashbox carousel-cell">
         <div class="coin-label">
             <img class="coin-logo" src="images/Ethereum-logo.png" alt="Ethereum Logo">
             <h4>ETH</h4>
+        </div>
+        <div class="ticker-main">
+            <h3>$164.00</h3>
+            <h4 class="change">+8.43%</h4>
+            <h4>Balance: 24.3758</h4>
         </div>
     </div>
 </div>
