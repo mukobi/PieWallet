1652aaafff08d362688930ed7f9071849541a7d7
Mukobi <gabrielmukobi@gmail.com>
2018-08-18 23:04:46 -0700

    Setup coin label next to image

--- components/dashboard/coinwidgets.php
+++ components/dashboard/coinwidgets.php
@@ -1,14 +1,20 @@
 <div id="coin-widget-container" class="main-carousel js-flickity" data-flickity-options='{"wrapAround": true, "watchCSS": true, "setGallerySize": false, "pageDots": false}'>
     <div id="widget-bitcoin" class="coin-widget dashbox carousel-cell">
-        <img class="coin-logo" src="images/Bitcoin-logo.png" alt="Bitcoin Logo">
-        <h3>BTC</h3>
+        <div class="coin-label">
+            <img class="coin-logo" src="images/Bitcoin-logo.png" alt="Bitcoin Logo">
+            <h4>BTC</h34>
+        </div>
     </div>
     <div id="widget-litecoin" class="coin-widget dashbox carousel-cell">
-        <img class="coin-logo" src="images/Litecoin-logo.png" alt="Litecoin Logo">
-        <h3>LTC</h3>
+        <div class="coin-label">
+            <img class="coin-logo" src="images/Litecoin-logo.png" alt="Litecoin Logo">
+            <h4>LTC</h4>
+        </div>
     </div>
     <div id="widget-ethereum" class="coin-widget dashbox carousel-cell">
-        <img class="coin-logo" src="images/Ethereum-logo.png" alt="Ethereum Logo">
-        <h3>ETH</h3>
+        <div class="coin-label">
+            <img class="coin-logo" src="images/Ethereum-logo.png" alt="Ethereum Logo">
+            <h4>ETH</h4>
+        </div>
     </div>
 </div>
