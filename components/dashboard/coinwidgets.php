f22ff93b8cee0157dbb886548bb72906a30d9c9c
Mukobi <gabrielmukobi@gmail.com>
2018-08-27 12:46:38 -0700

    Make coin widget background better and copy to three widgets

--- components/dashboard/coinwidgets.php
+++ components/dashboard/coinwidgets.php
@@ -4,34 +4,35 @@
             <div class="title"><span>BITCOIN</span><span>$8,573.00</span></div>
             <p>+1.52%</p>
             <p>1.3122314 ($8635.64)</p>
-            <div class="img"><img src="images/coins/bitcoin-trans.png" /></div>
-            
+            <img src="images/coins/bitcoin-trans.png" />
         </div>
         <div class="buttons">
             <a href="#" class="button">Send</a>
             <a href="#" class="button">Receive</a>
         </div>
     </div>
-    <div id="widget-litecoin" class="coin-widget dashbox carousel-cell">
-        <div class="coin-label">
-            <img class="coin-logo" src="images/Litecoin-logo.png" alt="Litecoin Logo">
-            <h4>LTC</h4>
+    <div id="widget-litecoin" class="coin-widget carousel-cell">
+        <div class="ticker-main dashbox">
+            <div class="title"><span>BITCOIN</span><span>$8,573.00</span></div>
+            <p>+1.52%</p>
+            <p>1.3122314 ($8635.64)</p>
+            <img src="images/coins/bitcoin-trans.png" />
         </div>
-        <div class="ticker-main">
-            <h3>$164.00</h3>
-            <h4 class="change">+2.73%</h4>
-            <h4>Balance: 98.2857</h4>
+        <div class="buttons">
+            <a href="#" class="button">Send</a>
+            <a href="#" class="button">Receive</a>
         </div>
     </div>
-    <div id="widget-ethereum" class="coin-widget dashbox carousel-cell">
-        <div class="coin-label">
-            <img class="coin-logo" src="images/Ethereum-logo.png" alt="Ethereum Logo">
-            <h4>ETH</h4>
+    <div id="widget-ethereum" class="coin-widget carousel-cell">
+        <div class="ticker-main dashbox">
+            <div class="title"><span>BITCOIN</span><span>$8,573.00</span></div>
+            <p>+1.52%</p>
+            <p>1.3122314 ($8635.64)</p>
+            <img src="images/coins/bitcoin-trans.png" />
         </div>
-        <div class="ticker-main">
-            <h3>$164.00</h3>
-            <h4 class="change">+8.43%</h4>
-            <h4>Balance: 24.3758</h4>
+        <div class="buttons">
+            <a href="#" class="button">Send</a>
+            <a href="#" class="button">Receive</a>
         </div>
     </div>
 </div>
