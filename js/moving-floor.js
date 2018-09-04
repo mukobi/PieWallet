9c6516963f8b3e4a3354109da9f69afa1fe038e5
Mukobi <gabrielmukobi@gmail.com>
2018-09-03 17:20:11 -0700

    Setup most of login page floor effect with vertical lines

---
+++ js/moving-floor.js
@@ -0,0 +1,28 @@
+function resizeAndDrawMovingFloor() {
+    var canvas = document.getElementById("moving-floor-canvas");
+    var canvasContainer = document.getElementById("moving-floor-canvas-container");
+    var maxWidth = canvasContainer.offsetWidth;
+    var maxHeight = canvasContainer.offsetHeight;
+    canvas.setAttribute("width", maxWidth);
+    canvas.setAttribute("height", maxHeight);
+
+    
+    var c=document.getElementById("moving-floor-canvas");
+    var ctx=c.getContext("2d");
+
+    ctx.strokeStyle = "white";
+    ctx.beginPath();
+
+    var numberVerticalReys = 37;
+    for(var i = 0; i <= numberVerticalReys; ++i) {
+        ctx.moveTo(maxWidth / 2, 0);
+        ctx.lineTo(maxWidth * i / numberVerticalReys,maxHeight);
+        ctx.stroke();
+    }
+
+    
+}
+resizeAndDrawMovingFloor();
+window.addEventListener('resize', resizeAndDrawMovingFloor);
+
+
--- login.php
+++ login.php
@@ -13,17 +13,30 @@ echo '<?xml version="1.0" encoding="utf-8"?>' ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN"
 "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-	<head>
-		<title>Login - PayPeer</title>
-		<link rel="stylesheet" href="/css/style11.css">
-		<link rel="stylesheet" href="/css/login.css">
-		<!-- <link rel="stylesheet" href="/css/buybox11.css"> -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script defer src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/TweenMax-latest-beta.js"></script>
-        <script defer src="js/gradient-color-change.js"></script>
-	</head>
-	<body>
-		<div id="main-container" class="content-main login">
-		</div>
-	</body>
+
+<head>
+    <title>Login - PayPeer</title>
+    <link rel="stylesheet" href="/css/style11.css">
+    <link rel="stylesheet" href="/css/login.css">
+    <!-- <link rel="stylesheet" href="/css/buybox11.css"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/TweenMax-latest-beta.js"></script>
+    <script defer src="js/gradient-color-change.js"></script>
+</head>
+
+<body>
+    <div id="main-container" class="content-main login">
+        <div id="moving-floor-canvas-container">
+            <canvas id="moving-floor-canvas">
+            </canvas>
+        </div>
+        <script defer src="js/moving-floor.js"></script>
+        <div class="login-box">
+            <img src="images/piewallet-long-logo.png" alt="Piewallet">
+            <div>
+                Login With Telegram
+            </div>
+        </div>
+    </div>
+</body>
 </html>
