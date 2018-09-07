9ebc1567592bc385b008feb25a35ca4b92955fb1
Mukobi <gabrielmukobi@gmail.com>
2018-09-06 17:16:01 -0700

    Rename telegram login variables

--- server/components/handleTgLogin.php
+++ server/components/handleTgLogin.php
@@ -20,12 +20,12 @@ function getTelegramUserData() {
 
 $tg_user = getTelegramUserData();
 if ($tg_user !== false) {
-    $id = (isset($tg_user['id']) ? htmlspecialchars($tg_user['id']) : false);
-    $first_name = (isset($tg_user['id']) ? htmlspecialchars($tg_user['first_name']) : false);
-    $last_name = (isset($tg_user['last_name']) ? htmlspecialchars($tg_user['last_name']) : false);
-    $username = (isset($tg_user['username']) ? htmlspecialchars($tg_user['username']) : false);
-    $photo_url = (isset($tg_user['photo_url']) ? htmlspecialchars($tg_user['photo_url']) : false);
-    $auth_date = (isset($tg_user['auth_date']) ? htmlspecialchars($tg_user['auth_date']) : false);
+    $tg_id = (isset($tg_user['id']) ? htmlspecialchars($tg_user['id']) : false);
+    $tg_first_name = (isset($tg_user['id']) ? htmlspecialchars($tg_user['first_name']) : false);
+    $tg_last_name = (isset($tg_user['last_name']) ? htmlspecialchars($tg_user['last_name']) : false);
+    $tg_username = (isset($tg_user['username']) ? htmlspecialchars($tg_user['username']) : false);
+    $tg_photo_url = (isset($tg_user['photo_url']) ? htmlspecialchars($tg_user['photo_url']) : false);
+    $tg_auth_date = (isset($tg_user['auth_date']) ? htmlspecialchars($tg_user['auth_date']) : false);
 } else {
     // no login cookie found
     header('Location: login.php');
