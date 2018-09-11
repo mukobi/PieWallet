f54c4804943ac56171fb46f57fbab39680271b8a
Mukobi <gabrielmukobi@gmail.com>
2018-09-11 11:38:41 -0700

    Add basic content and grid layout to account widget

--- components/account/accountwidget.php
+++ components/account/accountwidget.php
@@ -1,6 +1,23 @@
-<div id="exchange-widget" class="dashbox">
-    <link rel="stylesheet" href="../../css/profilewidget.css" />
-    <div>
-
+<div id="account-widget" class="dashbox">
+    <link rel="stylesheet" href="../../css/widgets/accountwidget.css" />
+    <div class="picture" <?php echo ($tg_photo_url ? "style=\" background-image: url(" . $tg_photo_url . ");\"" : "") ?>>
+    </div>
+    <div class="profile-info">
+        <h3><?php echo ($tg_first_name . ($tg_last_name ? " " . $tg_last_name : "")) ?></h3>
+        <?php echo ($tg_username ? "<h4>@" . $tg_username ."</h4>" : "") ?>
+        <p>Followers: 536</p>
+        <p>Following: 75</p>
+    </div>
+    <div class="buttons">
+        <div class="settings">
+            <a href="#" class="button">Settings</a>
+        </div>
+        <div class="logout">
+            <a href="?logout=1" class="button">Logout</a>
+        </div>
+    </div>
+    <div class="search">
+    </div>
+    <div class="notifications">
     </div>
 </div>
