9c9555a6dcbb07b72437249e8f11319e96702a57
Mukobi <gabrielmukobi@gmail.com>
2018-09-12 00:00:57 -0700

    Change user search form http method to get

--- components/account/searchwidget.php
+++ components/account/searchwidget.php
@@ -1,41 +1,38 @@
 <div class="searchwidget">
-    <form action="?go" method="post">
+    <form action="?search" method="get">
         <h3>Find Users</h3>
-        <input type="text" name="nickname" class="form-control" placeholder="Search Users by Telegram Name">
-        <input type="submit" class="button" name="search" value="Search">
+        <input type="text" name="query" class="form-control" placeholder="Search Users by Telegram Name">
+        <input type="submit" class="button">
     </form>
     <div class="results">
         <?php
-        if(isset($_POST['search'])){
-            if(isset($_GET['go'])){
-                if(preg_match("/^[  a-zA-Z]+/", $_POST['nickname'])) {
-                $name = $_POST['nickname'];
+        if(isset($_GET['query'])){
+            if(preg_match("/^[  a-zA-Z]+/", $_GET['query'])) {
+            $name = $_GET['query'];
 
-                $stmt = " SELECT  * FROM ls_users WHERE nickname LIKE '%" . $name .  "%'; ";
-                $result = $conn->query($stmt);
-                while( $row = $result->fetch_assoc() ){
-                    $id = $row['id'];
-                    $firstname = $row['firstname'];
-                    $lastname = $row['lastname'];
-                    $name = $firstname . " " . $lastname;
-                    $nickname = $row['nickname'];
-                    $email = $row['email'];
-                    $label = $row['label'];
-                    $address = $row['address'];
+            $stmt = " SELECT  * FROM ls_users WHERE nickname LIKE '%" . $name .  "%'; ";
+            $result = $conn->query($stmt);
+            while( $row = $result->fetch_assoc() ){
+                $id = $row['id'];
+                $firstname = $row['firstname'];
+                $lastname = $row['lastname'];
+                $name = $firstname . " " . $lastname;
+                $nickname = $row['nickname'];
+                $email = $row['email'];
+                $label = $row['label'];
+                $address = $row['address'];
 
-                    echo "<ul class='search_results' >";
-                    echo "<li>
-                    <a class='search_result' href='profile.php?id=$id'>" . $nickname . "</a>
-                    </li>";
-                    echo "</ul>";
-                    }
-                }
-                else{
-                    echo  "<p>Please enter a search query</p>";
+                echo "<ul class='search_results' >";
+                echo "<li>
+                <a class='search_result' href='profile.php?id=$id'>" . $nickname . "</a>
+                </li>";
+                echo "</ul>";
                 }
             }
+            else{
+                echo  "<p>Please enter a search query</p>";
+            }
         }
-
         ?>		
     </div>	
 </div>
