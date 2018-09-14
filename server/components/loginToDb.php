f66778a1cdbe14cc0ad049d4eb4d514f22185cec
Mukobi <gabrielmukobi@gmail.com>
2018-09-14 16:13:46 -0700

    Prefix db login when on 000webhost

--- server/components/loginToDb.php
+++ server/components/loginToDb.php
@@ -1,4 +1,8 @@
 <?php 
-$conn = new mysqli("localhost", "paypeer1_lite1", "wwOpF+T3bDl&", "piewallet_social"); 
-
+if(strpos($_SERVER['SERVER_NAME'], "000webhost") !== false) {
+    $conn = new mysqli("localhost", "id6641582_paypeer1_lite1", "wwOpF+T3bDl&", "id6641582_piewallet_social"); 
+}
+else {
+    $conn = new mysqli("localhost", "paypeer1_lite1", "wwOpF+T3bDl&", "piewallet_social"); 
+}
 ?>
