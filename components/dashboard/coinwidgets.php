fff1ae98a3f41b6d84170ec5693d3b61ed622035
Mukobi <gabrielmukobi@gmail.com>
2018-10-03 21:05:56 -0700

    Style coin widgets with new design

--- components/dashboard/coinwidgets.php
+++ components/dashboard/coinwidgets.php
@@ -1,9 +1,8 @@
 <div id="coin-widget-container" class="main-carousel js-flickity" data-flickity-options='{"wrapAround": true, "watchCSS": true, "setGallerySize": false, "pageDots": false}'>
     <div id="widget-bitcoin" class="coin-widget carousel-cell">
         <div class="ticker-main dashbox">
-            <div class="title"><span>BITCOIN</span><span>$7535.35</span></div>
-            <p>+1.52%</p>
-            <p>1.3122314 ($8635.64)</p>
+            <p class="title">1.3122314 BTC</p>
+            <p>$8635.64</p>
             <img src="images/coins/bitcoin-trans.png" />
         </div>
         <div class="buttons">
@@ -13,9 +12,8 @@
     </div>
     <div id="widget-litecoin" class="coin-widget carousel-cell">
         <div class="ticker-main dashbox">
-            <div class="title"><span>LITECOIN</span><span>$7535.35</span></div>
-            <p>+1.52%</p>
-            <p>1.3122314 ($8635.64)</p>
+            <p class="title">24.36478 LTC</p>
+            <p>$1369.08</p>
             <img src="images/coins/litecoin-trans.png" />
         </div>
         <div class="buttons">
@@ -25,9 +23,8 @@
     </div>
     <div id="widget-ethereum" class="coin-widget carousel-cell">
         <div class="ticker-main dashbox">
-            <div class="title"><span>ETHEREUM</span><span>$7535.35</span></div>
-            <p>+1.52%</p>
-            <p>1.3122314 ($8635.64)</p>
+            <p class="title">3.87674 ETH</p>
+            <p>$41,345.98</p>
             <img src="images/coins/ethereum-trans.png" />
         </div>
         <div class="buttons">
