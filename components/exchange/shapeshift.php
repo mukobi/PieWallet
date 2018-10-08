995f0b7ac655611ade0bd513ccb7f06950c06f1c
Mukobi <gabrielmukobi@gmail.com>
2018-10-08 14:54:56 -0700

    Finish styling the rest of exchange page

--- components/exchange/shapeshift.php
+++ components/exchange/shapeshift.php
@@ -1,3 +1,3 @@
-<div id="shapeshift" class="tab">
+<div id="shapeshift" class="tab" style="display:none">
     <iframe src="https://litespeed-uc2.herokuapp.com/#/?youHave_c=ETH&youGet_c=LTC" width="100%" height="100%" class="shapeshift" scrolling="auto" style="overflow-x: hidden; border: none" > Can't load widget </iframe>
 </div>
