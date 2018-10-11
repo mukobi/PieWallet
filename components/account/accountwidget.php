d575fd2ca5ff3f024958126eaea087c501c38cb2
Mukobi <gabrielmukobi@gmail.com>
2018-10-11 15:37:41 -0700

    Do most basic formatting for account redesign

--- components/account/accountwidget.php
+++ components/account/accountwidget.php
@@ -1,36 +1,36 @@
 <div id="account-widget" class="dashbox">
     <link rel="stylesheet" href="../../css/widgets/accountwidget.css" />
-    <div class="picture" <?php echo ($tg_photo_url ? "style=\" background-image: url(" . $tg_photo_url . ");\"" : "") ?>>
-    </div>
     <div class="profile-info">
-        <h3><?php echo ($tg_first_name . ($tg_last_name ? " " . $tg_last_name : "")) ?></h3>
-        <?php echo ($tg_username ? "<a href='https://t.me/" . $tg_username . "' class='link-element username'>@" . $tg_username ."</a>" : "") ?>
-        <br>
-        <p class="link-element follow-count" onClick="popUpFollows('followers', <?php echo $tg_id ?>)">Followers: <?php echo count($myFollowers) ?></p>
-        <br>
-        <p class="link-element follow-count" onClick="popUpFollows('following', <?php echo $tg_id ?>)">Following: <span id="follow-count"><?php echo count($myFollowing) ?></span></p>
-        <script>
-        function popUpFollows(type, id) {
-            changePopupWindowContents("");
-            getFollowsHTML(type, id, changePopupWindowContents);
-            showPopupWindow();
-        }
-        </script>
+        <div class="picture">
+            <img src='<?php echo ($tg_photo_url ? $tg_photo_url . "';" : "../images/users/genericprofile.png") ?>' />
+        </div>
+        <div id="account-info">
+            <h4><?php echo ($tg_first_name . ($tg_last_name ? " " . $tg_last_name : "")) ?></h4>
+            <div class="follow-count-section">
+                <p class="link-element follow-count" onClick="popUpFollows('followers', <?php echo $tg_id ?>)"><?php echo count($myFollowers) ?><br>followers</p>
+                <p class="link-element follow-count" onClick="popUpFollows('following', <?php echo $tg_id ?>)"><span id="follow-count"><?php echo count($myFollowing) ?></span><br>following</p>
+                <script>
+                function popUpFollows(type, id) {
+                    changePopupWindowContents("");
+                    getFollowsHTML(type, id, changePopupWindowContents);
+                    showPopupWindow();
+                }
+                </script>
+            </div>
+        </div>
+        <div id="balance">
+            Balance:<br>
+            $864.253
+        </div>
+        <div id="telegram">
+            Telegram:<br>
+            <?php echo ($tg_username ? "<a href='https://t.me/" . $tg_username . "' class='link-element username'>@" . $tg_username ."</a>" : "") ?>
+        </div>
     </div>
     <div class="buttons">
-        <div>
-            <a href="#" class="button">Wallets</a>
-            <a href="#" class="button">Settings</a>
-        </div>
-        <div>
-            <a href="?logout=1" class="button">Logout</a>
-        </div>
+        <a href="#" class="button">Wallets</a>
+        <a href="#" class="button">Settings</a>
+        <a href="#" class="button">Friends</a>
     </div>
-    <div class="search dashbox">
-        <?php include('searchwidget.php'); ?>
-    </div>
-    <div class="notifications dashbox">
-        <?php include('notificationswidget.php'); ?>
-    </div>
-    <?php include('components/popup-window.php') ?>
+    <?php include("components/popup-window.php"); ?>
 </div>
