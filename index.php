02ffb4aa8c75b570667b00ae6b9abce1f07129fc
Mukobi <gabrielmukobi@gmail.com>
2018-10-11 23:19:22 -0700

    Fix send receive on dashboard

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>' ?>
 			<?php 
 				include_once("components/dashboard/transactions.php");
 				include_once("components/dashboard/profilewidget.php");
+				include_once("components/dashboard/sendreceive.php");
 				include_once("components/dashboard/coinwidgets.php");
 				include_once("components/dashboard/graph.php");
 				include_once("components/navigation.php");
