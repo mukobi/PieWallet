dddc0ca66efbe8721537dbc5a4c3d181ebdb2bb2
Mukobi <gabrielmukobi@gmail.com>
2018-11-03 15:08:32 -0700

    Make script for dynamically fetching different coin wallet pages

--- components/dashboard/wallets.php
+++ components/dashboard/wallets.php
@@ -3,52 +3,52 @@
 include_once('components/loginToDb.php');
 include_once('components/handleTgLogin.php');
 
-if(isset($_GET['action'])) { 
-
-	if($_POST['action'] == 'unfollow'){
-		$target_id = $_POST['id'];
-		settype($target_id, 'integer');
-		settype($tg_id, 'integer');
-
-		$checkFollowStmt = "SELECT * FROM `follows` WHERE (follower_id = '".$tg_id."' AND following_id = '".$target_id."')";
-		$result = $conn->query($checkFollowStmt);
-		$row = $result->fetch_object();
-		
-		if($row){
-			$stmt = " DELETE FROM `follows` WHERE (follower_id = '".$tg_id."' AND following_id = '".$target_id."')";
-			$result = $conn->query($stmt);
-			if(!$result) {
-			 echo "<script> alert('Some error occured while unfollowing'); </script>" ;
-			}else{
-			 echo "<script> alert('Account has been unfollowed'); </script>";
-			}
-		}			
+if(isset($_GET['coin'])) { 
+	if($_GET['coin'] == 'btc'){
+		$walletType = 'bitcoin';
+		$walletTitle = 'Bitcoin Wallet';
+		$coinAbbreviation = 'BTC';
+		$popupCode = '0';
 	}
-
-	if($_POST['action'] == 'follow'){
-		$target_id = $_POST['id'];
-		settype($target_id, 'integer');
-		settype($tg_id, 'integer');
-
-		$current_timestamp = date("Y-m-d H:i:s");
-
-		$checkFollowStmt = "SELECT * FROM `follows` WHERE (follower_id = '".$tg_id."' AND following_id = '".$target_id."')";
-		$result = $conn->query($checkFollowStmt);
-		$row = $result->fetch_object();
-
-		if(!$row){
-			$stmt = " INSERT into follows (follower_id, following_id, follow_date) VALUES ($tg_id ,$target_id, ' ".$current_timestamp." '); ";
-			$result = $conn->query($stmt);
-			if(!$result) {
-			 echo "<script> alert('Some error occured while following'); </script>" ;
-			}else{
-			 echo "<script> alert('Account has been followed'); </script>";
-			}
-		}	
+	else if($_GET['coin'] == 'ltc'){
+		$walletType = 'litecoin';
+		$walletTitle = 'Litecoin Wallet';
+		$coinAbbreviation = 'LTC';
+		$popupCode = '1';
+	}
+	else if($_GET['coin'] == 'eth'){
+		$walletType = 'ethereum';
+		$walletTitle = 'Ethereum Wallet';
+		$coinAbbreviation = 'ETH';
+		$popupCode = '2';
+	}
+	else {	
+		echo "Sorry, something went wrong.";
 	}
 }
 else {
-	header('Location:../index.php');
+	echo "Sorry, something went wrong.";
 }
-
 ?>
+
+<div id="coin-widget-container" class="wallet-view">
+    <div id="widget-<?php echo $walletType;?>" class="coin-widget">
+        <div class="ticker-main dashbox">
+            <p class="title">1.3122314 <?php echo $coinAbbreviation;?></p>
+            <p>$8635.64</p>
+            <img src="images/coins/<?php echo $walletType;?>-trans.png" />
+        </div>
+        <div class="buttons">
+            <a onClick="popupSendReceive(<?php echo $popupCode;?>,'send')"><img src="images/icons/send.png" />Send</a>
+            <a onClick="popupSendReceive(<?php echo $popupCode;?>,'receive')"><img src="images/icons/receive.png" />Receive</a>
+        </div>
+    </div>
+    <script>
+        function popupSendReceive(coin, action) {
+            document.getElementById("send-receive-coin-select").selectedIndex = coin;
+            changeSendReceiveTab(action);
+            document.getElementById("sendreceive").classList.remove("transparent");
+            document.getElementById("sendreceive").classList.add("active");
+        }
+    </script>
+</div>
