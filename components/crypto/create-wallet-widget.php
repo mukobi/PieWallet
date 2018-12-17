1f04210876d721a28312da33b8694dbaf5534559
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-12-17 00:37:06 -0800

    Style generate wallet 2nd page

--- components/crypto/create-wallet-widget.php
+++ components/crypto/create-wallet-widget.php
@@ -1,5 +1,5 @@
 <div id="create-wallet-widget">
-    <div id="wallet-create-window-0" class="wallet-create-window active">
+    <div class="wallet-create-window active">
         <h3>Let's Create Your PieWallet!</h3>
         <p>These are your seed of 12 random words from which we generate your wallet. This way, you can write down easy to remember information that can be used to unlock your wallet it the future.</p>
         <div class="row">
@@ -9,12 +9,14 @@
         </div>
         <p>If you want new words, select "Random Words." When you are happy with your seed words, <b>be sure to write them down</b> then select "Generate Wallet."</p>
     </div>
-    <div id="wallet-create-window-1" class="wallet-create-window">
+    <div class="wallet-create-window">
         <h3>Your Private Key</h3>
         <p id="wallet-create-private-key"></p>
-        <p>This is your private key. It is very important that you <b>DO NOT LOSE THIS KEY</b>, as otherwise you will not be able to use any of the funds in your wallet.
+        <p>This is your private key. It is very important that you <b>DO NOT LOSE THIS KEY</b>, as otherwise you will not be able to use any of the funds in your wallet.</p>
+        <p>You private key never gets sent to PayPeer's servers or stored on PayPeer's database. It is up to you to keep a secure, offline copy of it, so <b>write it down on paper</b> then click "Addresses" to see your public addresses.</p>
+        <a class="btn primary" onClick="showPublicAddresses()">Addresses</a>
     </div>
-    <div id="wallet-create-window-2" class="wallet-create-window">
+    <div class="wallet-create-window">
     </div>
 </div>
 <script src="../../js/crypto/bip39.js"></script>
