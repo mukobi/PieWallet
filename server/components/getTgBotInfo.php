ffa9335120cae3a25153fc1c48109c2a3d86f904
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-12-19 17:42:42 -0800

    Change getTgBotInfo to check server name instead of env vars

--- server/components/getTgBotInfo.php
+++ server/components/getTgBotInfo.php
@@ -1,10 +1,10 @@
 <?php
-if(getenv('ENVIRONMENT') == 'DEV') {
-    define('TG_BOT_TOKEN', '637599184:AAFhVYf-dJEIVLqAWQWj4mk_RYE0vOuEcYk');
-    define('TG_BOT_NAME', 'PiewalletBot');
-}
-else {
+if(strpos($_SERVER['SERVER_NAME'], "000webhost") !== false) {  // test server
     define('TG_BOT_TOKEN', '628350776:AAGMY3nI9ctd-XlAeBQBSO38PSutOT9_lbM');
     define('TG_BOT_NAME', 'PayPeerBot');
+}
+else {  // dev
+    define('TG_BOT_TOKEN', '637599184:AAFhVYf-dJEIVLqAWQWj4mk_RYE0vOuEcYk');
+    define('TG_BOT_NAME', 'PiewalletBot');
 }
 ?>
