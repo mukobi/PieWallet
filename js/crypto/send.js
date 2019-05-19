b0f39a464662d633f5ce0530903a0d56fff4779e
Gabriel Mukobi <gabrielmukobi@gmail.com>
2019-05-18 22:48:42 -0700

    Add Send with Tx Skeleton Page and Layout

---
+++ js/crypto/send.js
@@ -0,0 +1,12 @@
+var changeSendTabs = function() {
+    var tabs = document.getElementsByClassName("tab");
+    for(var i = 0; i < tabs.length; i++) {
+        var tab = tabs[i];
+        if(tab.classList.contains("active")) {
+            tab.classList.remove("active");
+        }
+        else {
+            tab.classList.add("active");
+        }
+    }
+}
---
+++ send.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once("server/components/handleTgLogin.php");
+include_once("server/components/loginToDb.php");
+include_once("server/queryDbSocial.php");
+
+echo '<?xml version="1.0" encoding="utf-8"?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN"
+    "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+	<head>
+		<title>Send BTC - PayPeer.io</title>
+		<link rel="stylesheet" href="/css/style11.css">
+		<link rel="stylesheet" href="/css/generic-page.css">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<?php include_once("server/walletFunctionality.php"); ?>
+	</head>
+
+	<body>
+		<div id="main-container" class="content-main">
+			<?php
+			 	include_once("components/navigation.php");
+				include_once("components/crypto/send-widget.php");
+			?>
+		</div>
+	</body>
+</html>
