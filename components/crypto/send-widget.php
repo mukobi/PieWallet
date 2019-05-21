15d6a38d596110aad9aa47187b58512ab881d74e
Gabriel Mukobi <gabrielmukobi@gmail.com>
2019-05-21 00:02:14 -0700

    add send hint with link to unlock wallet

--- components/crypto/send-widget.php
+++ components/crypto/send-widget.php
@@ -5,9 +5,10 @@
         <div class="tab send-form active">
             <h3>Send Bitcoin</h3>
             <p>To Address: <input type="text" id="send-form-to-address" placeholder="Address to send BTC"></p>
-            <p><i>Hint: find the address of another user by <a href="search.php">searching for them on PieWallet</a></i></p>
+            <p><i>Hint: find the address of another user by <a href="search.php">searching for them on PieWallet.</a></i></p>
             <p>Your Address: <input type="text" id="send-form-from-address" placeholder="Your address"></p>
             <p>Your Private Key (WIF): <input type="text" id="send-form-from-private-key" placeholder="Your private key (WIF)"></p>
+            <p><i>Hint: <a href="unlockWallet.php">unlock your PieWallet</a> to find your address and private key.</i></p>
             <p>Amount: <input type="text" id="send-form-amount" placeholder="Amount in BTC"></p>
             <a class="btn primary search-button" onClick="sendTx()">Send</a>
         </div>
@@ -18,6 +19,7 @@
             <p></p>
         </div>
     </div>
-    <script src="../../js/crypto/txRequirementsBroswerified.js"></script>	
+    <script src="../../js/values/values.js"></script>	
+    <script src="../../js/crypto/txRequirementsBroswerified.js"></script>
     <script src="../../js/crypto/txSender.js"></script>	
 </div>
